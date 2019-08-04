03ede1d21b136ed80a2baefd4db6c4807692aaca
Aidan Fitzgerald <acf67@cornell.edu>
2019-08-04 04:55:19 -0700

    implement Fibonacci

---
+++ fibonacci/README.md
@@ -0,0 +1,5 @@
+# Fibonacci
+
+O(n) time, O(1) space implementation of the Fibonacci sequence.
+
+Since this implementation works with `u64` integers internally, it can only go up to the 93rd Fibonacci number (12,200,160,415,121,876,738).
--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -3,11 +3,35 @@ use std::io;
 fn main() {
     let input = read_int();
     println!("You entered: {}", input);
+
+    let output = fibonacci(input);
+    println!("The {} Fibonacci number is {}", ordinal(input), output)
 }
 
-fn read_int() -> u32 {
+fn fibonacci(n: u64) -> u64 {
+    if n <= 1 {
+        return n;
+    }
+    let mut values = [0, 1];
+    for i in 2..n+1 {
+        let index = (i % 2) as usize;
+        values[index] = values[0] + values[1];
+    }
+    values[(n % 2) as usize]
+}
+
+fn ordinal(i: u64) -> String {
+    match i {
+        1 => "1st".to_string(),
+        2 => "2nd".to_string(),
+        3 => "3rd".to_string(),
+        _ => format!("{}th", i),
+    }
+}
+
+fn read_int() -> u64 {
     loop {
-        println!("Enter a number:");
+        println!("Enter a number less than 94:");
         
         let mut line = String::new();
         io::stdin()
