c4ec26622d0b26a6b3f46bd43b20a535f7f2bce1
Aidan Fitzgerald <acf67@cornell.edu>
2019-08-04 02:07:05 -0700

    read in number and print it out

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_int();
+    println!("You entered: {}", input);
+}
+
+fn read_int() -> u32 {
+    loop {
+        println!("Enter a number:");
+        
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
+        
+        match line.trim().parse() {
+            Ok(num) => break num,
+            Err(_) => {
+                println!("Please type a non-negative integer.");
+                continue;
+            }
+        }
+    }
 }
