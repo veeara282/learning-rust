4daeb1e9764d2187fd72fc5bf190abb838b26553
Evelyn Fitzgerald <acf67@cornell.edu>
2020-05-24 01:13:14 -0400

    Implement mode

--- m-cubed/src/main.rs
+++ m-cubed/src/main.rs
@@ -1,15 +1,20 @@
 use rand;
 use rand::RngCore;
+use std::collections::HashMap;
 
 /// Computes the mean, median, and mode of a random sequence of integers.
 fn main() {
     let mut numbers = generate_integers(4096);
     numbers.sort_unstable();
+    println!("Generated 4096 integers.");
 
     let mean = mean(&numbers);
     let median = median(&numbers);
+    let mode = mode(&numbers).unwrap(); // cannot be None
 
-    println!("Hello, world!");
+    println!("Mean: {}", mean);
+    println!("Median: {}", median);
+    println!("Mode: {}", mode);
 }
 
 /// Generates `n` integers between 0 and 255.
@@ -23,6 +28,12 @@ fn generate_integers(n: usize) -> Vec<u32> {
 }
 
 /// Computes the mean of a sequence of integers.
+/// 
+/// Examples:
+/// ```
+/// let seq = vec![1, 2, 3];
+/// assert_eq!(mean(seq), 2.0);
+/// ```
 fn mean(ints: &Vec<u32>) -> f32 {
     let mut sum: f32 = 0.0;
     for x in ints {
@@ -42,4 +53,20 @@ fn median(ints: &Vec<u32>) -> f32 {
         let m2 = ints[n / 2] as f32;
         (m1 + m2) / 2.0
     }
+}
+
+/// Returns the mode of a sequence of integers, if one exists.
+/// If there are multiple modes, it may return any of them.
+/// Returns `None` if the input is empty.
+fn mode(ints: &Vec<u32>) -> Option<u32> {
+    let mut counts: HashMap<u32, u32> = HashMap::new();
+    for x in ints {
+        if let Some(count) = counts.get_mut(x) {
+            *count += 1;
+        } else {
+            counts.insert(*x, 1);
+        }
+    }
+    counts.iter().max_by_key(|(_val, count)| *count)
+                 .and_then(|(val, _count)| Some(*val))
 }
