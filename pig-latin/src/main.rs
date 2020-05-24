c87be8b0779bf66a9df0f21aa15c23d202477e7f
Evelyn Fitzgerald <acf67@cornell.edu>
2020-05-24 16:57:46 -0400

    Implement pig latin (doesn't handle digraphs yet)

--- pig-latin/src/main.rs
+++ pig-latin/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}, {}", pig_latin("hello"), pig_latin("world"));
+    let words_to_try = vec!["food", "social", "distancing", "pikachu", "spot", "squirtle"];
+    for word in words_to_try {
+        println!("{} → {}", word, pig_latin(word));
+    }
 }
+
+/// Converts the given word to pig Latin.
+/// Requires: The first character in the string is a letter.
+fn pig_latin(word: &str) -> String {
+    let mut word_chars = word.chars();
+    if let Some(first_letter) = word_chars.next() {
+        if is_vowel(first_letter) {
+            format!("{}-hay", word)
+        } else {
+            let affix = word_chars.as_str();
+            format!("{}-{}ay", affix, first_letter)
+        }
+    } else {
+        String::new()
+    }
+}
+
+fn is_vowel(letter: char) -> bool {
+    let letter = letter.to_ascii_lowercase();
+    letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'
+}
+
