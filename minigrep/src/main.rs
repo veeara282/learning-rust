ae7b1190ea363732a271435052c2622e8c81741a
Evelyn <evelyn.ciara.345@gmail.com>
2024-05-28 00:35:36 -0400

    parse args in own function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
 
+// struct GrepArgs<'a> {
+//     query: &'a str,
+//     file_path: &'a str,
+// }
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
@@ -14,4 +18,15 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+// fn parse_config_struct(args: &[String]) -> GrepArgs {
+//     GrepArgs { query: &args[1], file_path: &args[2] }
+// }
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    return (query, file_path)
 }
